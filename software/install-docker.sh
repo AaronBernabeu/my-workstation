9fbacbbd46ae7c964c44874fe41f95efa2ddfbe9
Aaron <aaronidasbr@gmail.com>
2021-10-24 18:48:48 +0200

    Update docker-compose version

--- software/install-docker.sh
+++ software/install-docker.sh
@@ -20,7 +20,7 @@ if ! location=$(type -p "docker"); then
 fi
 
 if ! location=$(type -p "docker-compose"); then
-    sudo curl -L "https://github.com/docker/compose/releases/download/1.25.5/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+    sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
     sudo chmod +x /usr/local/bin/docker-compose
 fi
 
