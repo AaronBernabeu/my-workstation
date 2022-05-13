054ed20b836d3b142788d2381900ec376e96c973
Aaron <aaronidasbr@gmail.com>
2022-05-13 19:16:26 +0200

    Update software for 22-04

--- software/install-jetbrains-toolbox.sh
+++ software/install-jetbrains-toolbox.sh
@@ -10,10 +10,10 @@ if ! test -f ~/.local/share/JetBrains/Toolbox/bin/jetbrains-toolbox; then
     SETUP_DIR="$(pwd)"
     cd /opt
 
-    sudo wget https://download-cf.jetbrains.com/toolbox/jetbrains-toolbox-1.20.8337.tar.gz
-    sudo tar -xzf jetbrains-toolbox-1.20.8337.tar.gz
-    sudo rm jetbrains-toolbox-1.20.8337.tar.gz
-    ./jetbrains-toolbox-1.20.8337/jetbrains-toolbox
+    sudo wget https://download-cdn.jetbrains.com/toolbox/jetbrains-toolbox-1.24.11947.tar.gz
+    sudo tar -xzf jetbrains-toolbox-1.24.11947.tar.gz
+    sudo rm jetbrains-toolbox-1.24.11947.tar.gz
+    ./jetbrains-toolbox-1.24.11947/jetbrains-toolbox
 
     cd ${SETUP_DIR}
 fi
