3f6f4baadbbc47ae90f6bcdece663c45cdcafd9e
Aaron <3294776+AaronBernabeu@users.noreply.github.com>
2026-05-02 22:36:31 +0200

    fix k9s install

--- software/install-k9s.sh
+++ software/install-k9s.sh
@@ -11,8 +11,7 @@ if ! command -v k9s &>/dev/null; then
     K9S_URL=$(curl -s https://api.github.com/repos/derailed/k9s/releases/latest \
       | grep -Po '"browser_download_url": *"\K[^"]*linux_'"$ARCH"'\.deb')
 
-    TMP_DIR=$(mktemp -d)
-    curl -fsSL "$K9S_URL" -o "$TMP_DIR/k9s.deb"
-    sudo apt install -y "$TMP_DIR/k9s.deb"
-    rm -rf "$TMP_DIR"
+    curl -fsSL "$K9S_URL" -o /tmp/k9s.deb
+    sudo apt install -y /tmp/k9s.deb
+    rm -f /tmp/k9s.deb
 fi
