d623757130790b3a8520aaa21107558cd4ab8d52
Aaron <3294776+AaronBernabeu@users.noreply.github.com>
2026-05-04 23:05:33 +0200

    add telegram desktop

--- software/install-telegram.sh
+++ software/install-telegram.sh
@@ -12,3 +12,18 @@ if ! command -v telegram-desktop &>/dev/null; then
     sudo ln -sf /opt/Telegram/Telegram /usr/local/bin/telegram-desktop
     rm /tmp/telegram.tar.xz
 fi
+
+if [ ! -f /usr/share/applications/telegram-desktop.desktop ]; then
+    sudo tee /usr/share/applications/telegram-desktop.desktop > /dev/null <<EOF
+[Desktop Entry]
+Name=Telegram Desktop
+Comment=Official desktop app for the Telegram messaging service
+Exec=/opt/Telegram/Telegram -- %u
+Icon=/opt/Telegram/telegram.png
+Terminal=false
+Type=Application
+Categories=Network;InstantMessaging;Qt;
+MimeType=x-scheme-handler/tg;
+StartupWMClass=TelegramDesktop
+EOF
+fi
